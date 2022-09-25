52eef629053dede55da85c49be499310a2de74aa
sergey <ser19061995@gmail.com>
2022-09-25 09:12:55 +0300

    сделал задание со словом страдание

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,11 @@ public class Main {
         MethodCountWord array = new MethodCountWord();
         ArrayList<String> lines = array.countScanner(file);
         System.out.println(lines.size());
+        for (int i=1;i<=10;i++) {
+            for (int j=1;j<=10;j++) {
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
     }
 }
